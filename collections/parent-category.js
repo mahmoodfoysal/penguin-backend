b3c830a8a4ad75121e2cadbd49fa6f144e8ba56d
FOYSAL\foysal <fmahmood033@gmail.com>
2024-12-28 17:04:35 +0600

    feat: add category status update

--- collections/parent-category.js
+++ collections/parent-category.js
@@ -16,13 +16,14 @@ const parentCategoryRoute = (parentCatCollection) => {
     //   post api 
     
     router.post("/api/admin/insert-update-parent-category", async (req, res) => {
-        const { _id, par_cat_id, par_cat_name } = req.body;
+        const { _id, par_cat_id, par_cat_name, status } = req.body;
   
         const data = {
           par_cat_id: typeof par_cat_id === "number" ? par_cat_id : null,
-          par_cat_name: typeof par_cat_name === "string" ? par_cat_name : null
+          par_cat_name: typeof par_cat_name === "string" ? par_cat_name : null,
+          status: typeof status === "number" ? status : null,
         };
-        if (data.par_cat_id === null || !data.par_cat_name ) {
+        if (data.par_cat_id === null || !data.par_cat_name || data.status === null ) {
           return res
             .status(404)
             .send({ error: "Invalid or missing required fields" });
@@ -55,6 +56,24 @@ const parentCategoryRoute = (parentCatCollection) => {
         }
       });
 
+      router.patch('/api/admin/update-parent-category-status/:id', async (req, res) => {
+        try {
+          const id = req.params.id;
+          const filter = { _id : new ObjectId(id) };
+          const {status} = req.body;
+          const updateDoc = {
+            $set: {status}
+          }
+          const result = await parentCatCollection.updateOne(filter, updateDoc);
+          res.status(201).send({
+            message: status === 1 ? 'Category active successful' : 'Category inactive successful'
+          });
+        }
+        catch(error) {
+          res.status(500).send({ message: "An error occurred while updating the status." });
+        }
+      });
+      
       return router;
 };
 
--- collections/sub-category.js
+++ collections/sub-category.js
@@ -3,62 +3,99 @@ const { ObjectId } = require("mongodb");
 const router = express.Router();
 
 const subCategoryRoute = (subCategoryCollection) => {
-    // get api 
-        router.get('/api/admin/get-sub-category', async(req, res) => {
-            const getSubCategory = subCategoryCollection.find();
-            const result = await getSubCategory.toArray();
-            res.send({
-              list_data: result,
-              message: 'Successful'
-            });
-          });
-    
-        //   post api 
-        
-        router.post("/api/admin/insert-update/sub-categoty", async (req, res) => {
-            const { _id, par_cat_id, sub_cat_id, par_cat_name,  sub_cat_name, userInfo } = req.body;
-      
-            const data = {
-              par_cat_id: typeof par_cat_id === "number" ? par_cat_id : null,
-              sub_cat_id: typeof sub_cat_id === "number" ? sub_cat_id : null,
-              par_cat_name: typeof par_cat_name === "string" ? par_cat_name : null,
-              sub_cat_name: typeof sub_cat_name === "string" ? sub_cat_name : null,
-              userInfo: typeof userInfo === "string" ? userInfo : null
-            };
-            if (data.par_cat_id === null || data.sub_cat_id === null || !data.sub_cat_name || !data.userInfo || !data.par_cat_name ) {
-              return res
-                .status(404)
-                .send({ error: "Invalid or missing required fields" });
-            }
-            try {
-              if (_id) {
-                const catId = new ObjectId(_id);
-                const result = await subCategoryCollection.updateOne(
-                  {
-                    _id: catId,
-                  },
-                  {
-                    $set: data,
-                  }
-                );
-                if (result.modifiedCount === 0) {
-                  return res.status(400).send({ message: "No data modified" });
-                }
-                res
-                  .status(201)
-                  .send({ message: "Update Successful", id: _id });
-              } else {
-                const result = await subCategoryCollection.insertOne(data);
-                res
-                  .status(201)
-                  .send({ message: "Successful", id: result.insertedId });
-              }
-            } catch (error) {
-              res.status(500).send({ error: "Failed to create or update category" });
-            }
-          });
-    
-          return router;
+  // get api
+  router.get("/api/admin/get-sub-category", async (req, res) => {
+    const getSubCategory = subCategoryCollection.find();
+    const result = await getSubCategory.toArray();
+    res.send({
+      list_data: result,
+      message: "Successful",
+    });
+  });
+
+  //   post api
+
+  router.post("/api/admin/insert-update/sub-categoty", async (req, res) => {
+    const {
+      _id,
+      par_cat_id,
+      sub_cat_id,
+      par_cat_name,
+      sub_cat_name,
+      userInfo,
+      status
+    } = req.body;
+
+    const data = {
+      par_cat_id: typeof par_cat_id === "number" ? par_cat_id : null,
+      sub_cat_id: typeof sub_cat_id === "number" ? sub_cat_id : null,
+      par_cat_name: typeof par_cat_name === "string" ? par_cat_name : null,
+      sub_cat_name: typeof sub_cat_name === "string" ? sub_cat_name : null,
+      userInfo: typeof userInfo === "string" ? userInfo : null,
+      status: typeof status === "number" ? status : null,
+    };
+    if (
+      data.par_cat_id === null ||
+      data.sub_cat_id === null ||
+      !data.sub_cat_name ||
+      !data.userInfo ||
+      !data.par_cat_name ||
+      data.status === null
+    ) {
+      return res
+        .status(404)
+        .send({ error: "Invalid or missing required fields" });
+    }
+    try {
+      if (_id) {
+        const catId = new ObjectId(_id);
+        const result = await subCategoryCollection.updateOne(
+          {
+            _id: catId,
+          },
+          {
+            $set: data,
+          }
+        );
+        if (result.modifiedCount === 0) {
+          return res.status(400).send({ message: "No data modified" });
+        }
+        res.status(201).send({ message: "Update Successful", id: _id });
+      } else {
+        const result = await subCategoryCollection.insertOne(data);
+        res.status(201).send({ message: "Successful", id: result.insertedId });
+      }
+    } catch (error) {
+      res.status(500).send({ error: "Failed to create or update category" });
+    }
+  });
+
+  router.patch(
+    "/api/admin/update-sub-category-status/:id",
+    async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const { status } = req.body;
+        const updateDoc = {
+          $set: { status },
+        };
+        const result = await subCategoryCollection.updateOne(filter, updateDoc);
+        res.status(201).send({
+          message:
+            status === 1
+              ? "Category active successful"
+              : "Category inactive successful",
+        });
+      } catch (error) {
+        res
+          .status(500)
+          .send({ message: "An error occurred while updating the status." });
+      }
+    }
+  );
+
+  return router;
 };
 
 module.exports = subCategoryRoute;
